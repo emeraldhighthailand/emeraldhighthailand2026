f911f4f2f146043945a2214d7ccbfa431a135faa
emeraldhighthailand <edwardstom689@gmail.com>
2026-02-16 15:24:59 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+const strains = [
+  {
+    name: "Gelato 41",
+    tier: "Street",
+    thc: "24%",
+    price: 900,
+    image: "assets/bud1.jpg",
+    description: "Citrus fuel aroma. Clean daily smoke."
+  },
+  {
+    name: "Jungle Runtz",
+    tier: "Signature",
+    thc: "27%",
+    price: 1200,
+    image: "assets/bud2.jpg",
+    description: "Candy gas terpenes. Strong bag appeal."
+  },
+  {
+    name: "Emerald Reserve #01",
+    tier: "Reserve",
+    thc: "29%",
+    price: 1800,
+    image: "assets/bud3.jpg",
+    description: "Limited 75 jars. Bangkok exclusive drop."
+  }
+];
